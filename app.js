07950ad9671174354a76c4485145191cd269a46d
kraftme90 <35199591+kraftme90@users.noreply.github.com>
2018-08-06 21:47:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const mysql = require('./dbcon.js');
 
 // init the view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main.handlebars.html'}));
@@ -9,11 +10,23 @@ app.set('view engine', 'hbs');
 
 // serving the static pages
 app.use(express.static('./public'));
+app.set('mysql', mysql);
 
 //serving the main page
 app.get('/', function(req, res, next)  {
    res.status(200);
    res.render('helloWorld.handlebars');
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 //setting the port to the env variable or 3000 as default
